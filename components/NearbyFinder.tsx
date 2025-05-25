705fae040092cbc64a5973dedd997e1a7c2dddd3
Sanskriti Gupta <sanskritigupta@Sanskritis-MacBook-Air.local>
2025-05-25 09:40:18 +0530

    railway app

--- components/NearbyFinder.tsx
+++ components/NearbyFinder.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://nearby-chats-production.up.railway.app", {
     transports: ["websocket"],
 });
 
