cecddc85b4905359e5d6648eed542c203f6fe8b6
Sanskriti Gupta <sanskritigupta@Sanskritis-MacBook-Air.local>
2025-05-25 09:44:51 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3001',
+        origin: 'https://sweet-zabaione-577224.netlify.app',
         methods: ['GET', 'POST'],
     }
 });
